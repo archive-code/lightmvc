cea23470091d63a25b867f31ec88a21a612fcb2c
johnny <johnny@jpixy.com>
2017-11-01 15:27:37 +0800

    update

--- src/main/java/jpixy/com/framework/util/CastUtil.java
+++ src/main/java/jpixy/com/framework/util/CastUtil.java
@@ -1,5 +1,7 @@
 package jpixy.com.framework.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class CastUtil {
 
     public static String castString(Object obj) {
@@ -10,5 +12,75 @@ public class CastUtil {
         return obj != null ? String.valueOf(obj) : defaultValue;
     }
 
-    public
+    public static double castDouble(Object obj){
+        return CastUtil.castDouble(obj, 0);
+    }
+
+    public static double castDouble(Object obj, double defaultValue){
+        double doubleValue = defaultValue;
+        if (obj != null){
+            String strValue = castString(obj);
+            if(StringUtils.isNotEmpty(strValue)){
+                try{
+                    doubleValue = Double.parseDouble(strValue);
+                }catch (NumberFormatException e){
+                    doubleValue = defaultValue;
+                }
+            }
+        }
+
+        return doubleValue;
+    }
+
+    public static long castLong(Object obj){
+        return CastUtil.castLong(obj, 0);
+    }
+
+    public static long castLong(Object obj, long defaultValue){
+        long longValue = defaultValue;
+        if (obj != null){
+            String strValue = castString(obj);
+            if(StringUtils.isNotEmpty(strValue)){
+                try{
+                    longValue = Long.parseLong(strValue);
+                }catch (NumberFormatException e){
+                    longValue = defaultValue;
+                }
+            }
+        }
+
+        return longValue;
+    }
+
+    public static int castInt(Object obj){
+        return CastUtil.castInt(obj, 0);
+    }
+
+    public static int castInt(Object obj, int defaultValue){
+        int intValue = defaultValue;
+        if(obj != null){
+            String strValue = castString(obj);
+            if(StringUtils.isNotEmpty(strValue)){
+                try{
+                    intValue=Integer.parseInt(strValue);
+                }catch (NumberFormatException e){
+                    intValue = defaultValue;
+                }
+            }
+        }
+        return intValue;
+    }
+
+    public static boolean castBoolean(Object obj){
+        return CastUtil.castBoolean(obj, false);
+    }
+
+    public static boolean castBoolean(Object obj, boolean defaultValue){
+        boolean booleanValue = defaultValue;
+        if(obj != null){
+            booleanValue = Boolean.parseBoolean(castString(obj));
+        }
+        return booleanValue;
+    }
+
 }
